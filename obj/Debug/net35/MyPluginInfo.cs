dbd18ee48036209aa8f1f82b725da3780b371f20
Cdemy Teilnehmer <cdemy@teilnehmer.cdemy.de>
2025-11-22 23:10:47 +0100

    update

--- obj/Debug/net35/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MyAssemblyCSharpPatcher
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "MyAssemblyCSharpPatcher";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
